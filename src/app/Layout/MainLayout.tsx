6dcb2fa616e3009666b248259aa84294a59d7925
Hendry Widyanto <hendrywidyanto97@gmail.com>
2024-05-05 23:53:39 -0700

    Change 6

--- src/app/Layout/MainLayout.tsx
+++ src/app/Layout/MainLayout.tsx
@@ -3,13 +3,13 @@ import "./MainLayout.css";
 
 function MainLayout({ children }: { children: any }) {
   return (
-    <div className="container-fluid">
+    <div className="">
       <NavBar />
       <div className="">
         {/* <div id="sideBar" className="col-md-2 collapse collapse-horizontal border-end">
                     <SideBar />
                 </div> */}
-        <div className="ms-1 me-1 mt-4 container-fluid">{children}</div>
+        <div className="ms-4 me-4 mt-4">{children}</div>
       </div>
     </div>
   );
